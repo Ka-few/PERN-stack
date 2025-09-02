7c3ea30df9a539332972b4e9c7a7a6766e09021c
ka-few <njorovista@gmail.com>
2025-09-02 14:38:10 +0300

    Setup the client side layout

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import { Fragment } from 'react/jsx-runtime';
+import './App.css';
+
+function App() {
+  return (
+   <div>
+    <Fragment></Fragment>;
+    </div>
+  );
+}
+
+export default App;
